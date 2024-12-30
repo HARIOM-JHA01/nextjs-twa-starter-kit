6c69515b3ae3bdae6f69b2ae1d80a4cf736c7a46
Hariom Jha <jhahariom040@gmail.com>
2024-12-30 19:27:57 +0530

    Refactor Home component and add conditional initialization of WebApp

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,36 @@
 "use client";
-import WebApp from "@twa-dev/sdk";
-import { useEffect, useState } from "react";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import WebApp from "@twa-dev/sdk";
+
+const Home: React.FC = () => {
+    const [count, setCount] = useState<number>(0);
+
     useEffect(() => {
-        WebApp.ready();
+        if (typeof window !== "undefined") {
+            WebApp.ready();
+        }
     }, []);
 
-    const [count, setCount] = useState(0);
+    const handleShowAlert = () => {
+        WebApp.showAlert("Hello World!");
+    };
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-4xl font-bold text-center text-blue-300">
-                Welcome to Next.Js
+                Welcome to Next.js
             </h1>
-            <div className=" p-3 m-2 bg-slate-300 hover:bg-slate-700 hover:text-white hover:font-bold rounded-md ">
-                <button onClick={() => setCount((count) => count + 1)}>
+            <div className="p-3 m-2 bg-slate-300 hover:bg-slate-700 hover:text-white hover:font-bold rounded-md">
+                <button onClick={() => setCount((prevCount) => prevCount + 1)}>
                     count is {count}
                 </button>
             </div>
-            <div className=" p-3 m-2 bg-slate-300 hover:bg-slate-700 hover:text-white hover:font-bold rounded-md ">
-                <button onClick={() => WebApp.showAlert(`Hello World!`)}>
-                    Show Alert
-                </button>
+            <div className="p-3 m-2 bg-slate-300 hover:bg-slate-700 hover:text-white hover:font-bold rounded-md">
+                <button onClick={handleShowAlert}>Show Alert</button>
             </div>
         </div>
     );
-}
+};
+
+export default Home;
